3a7ff3d2b790da1c9448e3e822efa58ef23a96f5
Ankur Mundra <amundra@ncsu.edu>
2023-03-30 02:15:52 -0400

    Dockerized the application and added bootstrap
    Signed-off-by: Ankur Mundra <20452032+mundra-ankur@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import Home from "./components/Layout/Home";
-import RootLayout from "./components/Layout/Root";
-import Users from "./components/Users/Users";
+import React from 'react';
 
 function App() {
-  const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <RootLayout />,
-      children: [
-        { index: true, element: <Home /> },
-        { path: "users", element: <Users /> },
-      ],
-    },
-  ]);
-
-  return <RouterProvider router={router} />;
+  return (
+      <div>
+      </div>
+  );
 }
 
 export default App;
