e36b14c53bc589d17aca96c52b4c9b57207a941d
Ankur Mundra <amundra@ncsu.edu>
2023-03-30 03:53:26 -0400

    User Component-->Show, Create, Update, Delete
    Signed-off-by: Ankur Mundra <20452032+mundra-ankur@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React from "react";
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./components/Layout/Home";
 import RootLayout from "./components/Layout/Root";
+import Users from "./components/Users/Users";
 
 function App() {
-    const router = createBrowserRouter([
-        {
-            path: "/",
-            element: <RootLayout/>,
-            children: [
-                {index: true, element: <Home/>},
-            ],
-        },
-    ]);
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <RootLayout />,
+      children: [
+        { index: true, element: <Home /> },
+        { path: "users", element: <Users /> },
+      ],
+    },
+  ]);
 
-    return <RouterProvider router={router}/>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
