b2ef8aa9119fb44b68ddc9c3cf59555e42b8deb3
Ankur Mundra <amundra@ncsu.edu>
2023-03-30 03:22:57 -0400

    Added Header for the application
    Signed-off-by: Ankur Mundra <20452032+mundra-ankur@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React from "react";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import Home from "./components/Layout/Home";
+import RootLayout from "./components/Layout/Root";
 
 function App() {
-  return (
-      <div>
-      </div>
-  );
+    const router = createBrowserRouter([
+        {
+            path: "/",
+            element: <RootLayout/>,
+            children: [
+                {index: true, element: <Home/>},
+            ],
+        },
+    ]);
+
+    return <RouterProvider router={router}/>;
 }
 
 export default App;
