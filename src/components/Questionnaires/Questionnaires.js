b8f2684ae5821c1a0ffc2b9c95ca550edce65f13
Michael Fergione <mefergio@ncsu.edu>
2023-04-23 18:21:26 -0400

    Display Working, Using Local JSON object

--- src/components/Questionnaires/Questionnaires.js
+++ src/components/Questionnaires/Questionnaires.js
@@ -9,7 +9,7 @@ import CreateQuestionnaire from "./CreateQuestionnaire";
 import DeleteQuestionnaire from "./DeleteQuestionnaire";
 import UpdateQuestionnaire from "./UpdateQuestionnaire";
 import {QUESTIONNAIRE_COLUMNS} from "./questionnaireColumns";
-//import record_data from './records.json'; // for testing
+import DATA from './records.json'; // for testing
 
 const Questionnaires = () => {
   const dispatch = useDispatch();
@@ -143,7 +143,7 @@ const Questionnaires = () => {
       </Row>
       <Row>
         <Table
-          data={tableData}
+          data={DATA}
           columns={tableColumns}
           initialState={initialState}
         />
